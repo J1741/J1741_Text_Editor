7b117a309db3eebd2c70718b7577b76794cd4672
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-07 19:58:18 -0500

    adding css loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,8 +53,24 @@ module.exports = () => {
     ],
 
     module: {
+      // ** css loaders**
       rules: [
-        
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+         test: /\.m?js$/,
+         exclude: /node_modules/,
+         // ** use babel-loader to use ES6
+         use: {
+           loader: 'babel-loader',
+           options: {
+             presets: ['@babel/preset-env'],
+             plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+           },
+         },
+       },
       ],
     },
   };
