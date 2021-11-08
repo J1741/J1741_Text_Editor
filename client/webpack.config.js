87b35ef6097eaa0f8d53899bce0cba69fd87a010
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-07 19:52:54 -0500

    adding code to generate a new WebpackPwaManifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,7 +31,25 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      
+
+      // ** creates a manifest.json file
+      new WebpackPwaManifest ({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
