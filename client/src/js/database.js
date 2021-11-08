31b8c8a29ccf1741228d719f4db60cee83b03054
J1741 <84889065+J1741@users.noreply.github.com>
2021-11-07 21:00:14 -0500

    adding logic to getDb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
 // ** export a function to post to the database **
-export const postDb = async (content) => {
+export const putDb = async (content) => {
   console.log('Post to the database');
 
   // ** create a connection to the database and version we want to use **
@@ -37,6 +37,27 @@ export const postDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// export const getDb = async () => console.error('getDb not implemented');
+// ** export a function to get all content from database **
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // ** create a connection to the database and version we want to use
+  const jateDb = await openDB('jate', 1);
+
+  // ** create a new transaction and specify db and privileges
+  const tx = jateDb.transaction('contact', 'readonly');
+
+  // ** open up the desired object store
+  const store = tx.objectStore('jate');
+
+  // ** use .getAll() method to get all data in the db
+  const request = store.getAll();
+
+  // ** get confirmation of request **
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
